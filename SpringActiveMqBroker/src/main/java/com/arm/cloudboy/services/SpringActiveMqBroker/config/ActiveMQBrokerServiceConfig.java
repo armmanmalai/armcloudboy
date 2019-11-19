a1740c85d9574d3677ad6af4ec0995fcb30dd65e
Arm Manmalai <arm.m04@gmail.com>
2019-11-19 23:54:56 +0700

    Active MQ with DATSOURCE And initial from spring bean

--- SpringActiveMqBroker/src/main/java/com/arm/cloudboy/services/SpringActiveMqBroker/config/ActiveMQBrokerServiceConfig.java
+++ SpringActiveMqBroker/src/main/java/com/arm/cloudboy/services/SpringActiveMqBroker/config/ActiveMQBrokerServiceConfig.java
@@ -1,22 +1,53 @@
 package com.arm.cloudboy.services.SpringActiveMqBroker.config;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.apache.activemq.xbean.BrokerFactoryBean;
+import org.apache.derby.jdbc.EmbeddedDataSource;
+import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.io.Resource;
 
 @Configuration
 public class ActiveMQBrokerServiceConfig {
-	 @Value("classpath:static/activemq-persit-off.xml")
+	    @Value("classpath:static/activemq-persit-on.xml")
 	    Resource mqRes;
+	   
+	    @Autowired
+	    private ApplicationContext appContext;
+	   
+	    //@Autowired
+	    DataSource mqDatasource ;
 	    
 	    @Bean(name="actvieMqEmbbedd")
-	    public BrokerFactoryBean brokerFactory(){
+	    @DependsOn({"amqDs"})
+	    public BrokerFactoryBean brokerFactory() throws SQLException{
 	    	BrokerFactoryBean brokerFactory = new BrokerFactoryBean() ;
+	    	//mqDatasource.getConnection().close();
 	    	Resource res  = mqRes;
 	    	brokerFactory.setConfig(res);
 	    	brokerFactory.setStart(true);
+	    	brokerFactory.setApplicationContext(appContext);
 	    	return brokerFactory;
 	    }
+	    
+	   
+	    @Bean("amqDs")
+	    public DataSource getEembededDerby(){
+
+	    	JdbcDataSource ds = new JdbcDataSource();
+	        ds.setURL("jdbc:h2:/Users/armm/TEST_ANYTHING/armAmq.db");
+	        ds.setUser("sa");
+	        return ds;
+	    }
+
 }
---
+++ SpringActiveMqBroker/src/main/java/com/arm/cloudboy/services/SpringActiveMqBroker/config/ActiveMQJmxConfig.java
@@ -0,0 +1,9 @@
+package com.arm.cloudboy.services.SpringActiveMqBroker.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActiveMQJmxConfig {
+	//jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi
+	
+}
